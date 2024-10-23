dda7051770b604f1aa6aa27609f779272f5e7134
maximus <u.maksim@telemart.ua>
2024-10-23 09:38:41 +0300

    windows forms project added

---
+++ Lesson5/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+try 
+{
+    SendMessageToViber(Console.ReadLine()!);
+}
+catch (ViberException ex)
+{
+    Console.WriteLine("Введений номер пустий");
+}
+catch
+{
+    Console.WriteLine("Something went wrong");
+}
+
+
+static void SendMessageToViber(string phone)
+{
+    if (string.IsNullOrEmpty(phone))
+    {
+        throw new ViberException("Phone number is empty");
+    }
+}
+
+
+class ViberException : Exception
+{
+    public ViberException(string errorText)
+        : base(errorText)
+    {
+    }
+}
+
+
---
+++ Lesson5_WindowsForms/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Lesson5_WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            radioButton1 = new RadioButton();
+            checkedListBox1 = new CheckedListBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(207, 188);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(341, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(207, 313);
+            button1.Name = "button1";
+            button1.Size = new Size(341, 80);
+            button1.TabIndex = 1;
+            button1.Text = "Перевірка IBAN";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(571, 92);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(94, 19);
+            radioButton1.TabIndex = 2;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(250, 49);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(120, 94);
+            checkedListBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(661, 234);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 4;
+            label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(checkedListBox1);
+            Controls.Add(radioButton1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private RadioButton radioButton1;
+        private CheckedListBox checkedListBox1;
+        private Label label1;
+    }
+}
